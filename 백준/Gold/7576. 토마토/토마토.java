c5306126150dd235e3877f5063e572a74577ab28
JayPark <127723000+jaepar@users.noreply.github.com>
2025-03-06 17:50:09 +0900

    [Gold V] Title: 토마토, Time: 576 ms, Memory: 120228 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -4,29 +4,38 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
     static int M, N;
     static int[][] box;
-    static int[] side_x = {-1, 1, 0, 0};
-    static int[] side_y = {0, 0, -1, 1};
-    static int day = 0;
-    static Queue<int[]> q = new LinkedList<>();
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static Queue<Node> q = new LinkedList<>();
+    public static int day = 0;
+
+    public static class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
+
+        M = Integer.parseInt(st.nextToken());   // 열
+        N = Integer.parseInt(st.nextToken());   // 행
         box = new int[N][M];
 
         for (int i = 0; i < N; i++) {
-            StringTokenizer st_t = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                box[i][j] = Integer.parseInt(st_t.nextToken());
+                box[i][j] = Integer.parseInt(st.nextToken());
                 if (box[i][j] == 1) {
-                    q.add(new int[]{i, j});
+                    q.add(new Node(i, j));
                 }
             }
         }
@@ -35,42 +44,40 @@ public class Main {
 
         if (check()) {
             System.out.println(day - 1);
-        } else
+        } else {
             System.out.println(-1);
+        }
 
     }
 
     public static void bfs() {
         while (!q.isEmpty()) {
-            int[] t = q.poll();
-            int x = t[0];
-            int y = t[1];
+            Node node = q.poll();
+            int x = node.x;
+            int y = node.y;
 
             for (int i = 0; i < 4; i++) {
-                int now_x = x + side_x[i];
-                int now_y = y + side_y[i];
+                int now_x = x + dx[i];
+                int now_y = y + dy[i];
 
-                if (now_x >= 0 && now_y >= 0 && now_x < N && now_y < M) {
+                if (now_x >= 0 && now_x < N && now_y >= 0 && now_y < M) {
                     if (box[now_x][now_y] == 0) {
                         box[now_x][now_y] = box[x][y] + 1;
-                        q.add(new int[]{now_x, now_y});
+                        q.add(new Node(now_x, now_y));
                     }
                 }
             }
 
         }
-
     }
 
     public static boolean check() {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (box[i][j] == 0)
-                    return false;
+                if (box[i][j] == 0) return false;
                 day = Math.max(day, box[i][j]);
             }
         }
         return true;
     }
-
 }
