cb21c7f3e9663eed0f507646b323ddaf705c9b8b
JayPark <127723000+jaepar@users.noreply.github.com>
2025-03-14 15:40:38 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14572 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -6,24 +6,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        int[][] dp = new int[N][3];
 
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            dp[i][0] = Integer.parseInt(st.nextToken());
-            dp[i][1] = Integer.parseInt(st.nextToken());
-            dp[i][2] = Integer.parseInt(st.nextToken());
+        int home[][] = new int[N + 1][3];
+        int dp[][] = new int[N + 1][3];
+
+        for (int i = 1; i < N + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            home[i][0] = Integer.parseInt(st.nextToken());
+            home[i][1] = Integer.parseInt(st.nextToken());
+            home[i][2] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 1; i < N; i++) {
-            dp[i][0] += Math.min(dp[i - 1][1], dp[i - 1][2]);
-            dp[i][1] += Math.min(dp[i - 1][0], dp[i - 1][2]);
-            dp[i][2] += Math.min(dp[i - 1][0], dp[i - 1][1]);
+        dp[1][0] = home[1][0];
+        dp[1][1] = home[1][1];
+        dp[1][2] = home[1][2];
+
+        for (int i = 2; i < N + 1; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + home[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + home[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + home[i][2];
         }
 
-        System.out.println(Math.min(Math.min(dp[N - 1][0], dp[N - 1][1]), dp[N - 1][2]));
+        System.out.println(Math.min(dp[N][0], Math.min(dp[N][1], dp[N][2])));
     }
 
 }
