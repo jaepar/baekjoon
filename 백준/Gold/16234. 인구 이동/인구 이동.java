0639592d241c4fa5b5956197408294056ce3efb6
JayPark <127723000+jaepar@users.noreply.github.com>
2025-06-04 22:46:55 +0900

    [Gold IV] Title: 인구 이동, Time: 596 ms, Memory: 298260 KB -BaekjoonHub

---
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -0,0 +1,98 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, L, R;
+    static int[][] map;
+    static boolean[][] visited;
+    static boolean flag;
+    static ArrayList<Node> list = new ArrayList<>();
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int day = 0;
+
+    static class Node{
+        int x, y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        map = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        move();
+        System.out.println(day);
+    }
+
+    static void move() {
+        while (true) {
+            flag = false;
+            visited = new boolean[N][N];
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (!visited[i][j]) {
+                        bfs(i, j);
+                    }
+                }
+            }
+            if (!flag)
+                break;
+            else
+                day++;
+        }
+    }
+
+    static void bfs(int x, int y) {
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(x, y));
+        visited[x][y] = true;
+        list.add(new Node(x, y));
+
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
+                    if (!visited[nx][ny] && Math.abs(map[cur.x][cur.y] - map[nx][ny]) >= L && Math.abs(map[cur.x][cur.y] - map[nx][ny]) <= R) {
+                        flag = true;
+                        visited[nx][ny] = true;
+                        list.add(new Node(nx, ny));
+                        q.add(new Node(nx, ny));
+                    }
+                }
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < list.size(); i++) {
+            Node node = list.get(i);
+            sum += map[node.x][node.y];
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            Node node = list.get(i);
+            map[node.x][node.y] = sum / list.size();
+        }
+        list.removeAll(list);
+    }
+
+}
