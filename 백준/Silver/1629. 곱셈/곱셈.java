9f823360a6f1fe6893acb4059511db3de3e3922b
JayPark <127723000+jaepar@users.noreply.github.com>
2026-03-22 19:09:02 +0900

    [Silver I] Title: 곱셈, Time: 96 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	static long A, B, C;
+	
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		
+		System.out.println(pow(A, B));
+	}
+
+	private static long pow(long a, long b) {
+		
+		if (b == 1) {
+			return a % C;
+		}
+		
+		long half = pow(a, b / 2);
+		
+		if (b % 2 == 0) {
+			return (half * half) % C;
+		}
+		else {
+			return ((half * half) % C * a) % C;
+		}
+	}
+	
+
+}
