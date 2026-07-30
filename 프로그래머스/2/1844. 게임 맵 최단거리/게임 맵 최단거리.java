2ed1282ccce6a21ffa6c38f700f953769beb13a0
JayPark <127723000+jaepar@users.noreply.github.com>
2026-07-30 23:07:02 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.43 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,26 +1,23 @@
 import java.util.*;
 
 class Solution {
-    
+    static int n, m;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int n, m;
-    static boolean[][] visited;
+    static boolean[][] visit;
     
     public int solution(int[][] maps) {
         n = maps.length;
         m = maps[0].length;
-        visited = new boolean[n][m];
+        visit = new boolean[n][m];
         
-        return bfs(0, 0, maps);
-        
+        return(bfs(0, 0, maps));
     }
     
     private int bfs(int x, int y, int[][] maps) {
         Queue<int[]> q = new LinkedList<>();
-        
         q.add(new int[]{x, y, 1});
-        visited[x][y] = true;
+        visit[x][y] = true;
         
         while(!q.isEmpty()) {
             int[] cur = q.poll();
@@ -28,7 +25,7 @@ class Solution {
             int curY = cur[1];
             int cnt = cur[2];
             
-            if (curX == n -1 && curY == m - 1) {
+            if (curX == n - 1 && curY == m - 1){
                 return cnt;
             }
             
@@ -36,17 +33,20 @@ class Solution {
                 int nx = curX + dx[i];
                 int ny = curY + dy[i];
                 
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
                     continue;
+                }
                 
-                if (maps[nx][ny] == 0)
+                if (maps[nx][ny] == 0) {
                     continue;
+                }
                 
-                if (visited[nx][ny])
+                if (visit[nx][ny]) {
                     continue;
+                }
                 
-                visited[nx][ny] = true;
                 q.add(new int[]{nx, ny, cnt + 1});
+                visit[nx][ny] = true;
             }
         }
         return -1;
