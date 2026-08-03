97d531f65234cc21622eecebeab2006457f8aed0
JayPark <127723000+jaepar@users.noreply.github.com>
2026-08-03 19:31:37 +0900

    [level 2] Title: 빛의 경로 사이클, Time: 45.55 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/86052. 빛의 경로 사이클/빛의 경로 사이클.java
+++ 프로그래머스/2/86052. 빛의 경로 사이클/빛의 경로 사이클.java
@@ -1,20 +1,18 @@
 import java.util.*;
 
 class Solution {
-    private static int DIR_NUM = 4;
-    private int r, c;
-    
-    private int[][] value;
-    private int[] dx = {-1, 0, 1, 0};
-    private int[] dy = {0, 1, 0, -1};
-    private boolean[][][] visited;
-    private List<Integer> result = new ArrayList<>();
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] dir;
+    static boolean[][][] visited;
+    static int DIR_NUM = 4;
+    static int r, c;
+    static List<Integer> result = new ArrayList<>();
     
     public int[] solution(String[] grid) {
         r = grid.length;
-        c = grid[0].length();
-        
-        value = new int[r][c];
+        c = grid[0].length();        
+        dir = new int[r][c];
         visited = new boolean[r][c][DIR_NUM];
         
         init(grid);
@@ -22,6 +20,7 @@ class Solution {
         move();
         
         Collections.sort(result);
+        
         int[] answer = result.stream()
             .mapToInt(i -> i)
             .toArray();
@@ -56,27 +55,27 @@ class Solution {
             
             int nx = (nowX + dx[nowD] + r) % r;
             int ny = (nowY + dy[nowD] + c) % c;
-            
-            int nd = (nowD + value[nx][ny] + DIR_NUM) % DIR_NUM;
+            int nd = (nowD + dir[nx][ny] + DIR_NUM) % DIR_NUM;
             
             nowX = nx;
             nowY = ny;
             nowD = nd;
         }
-        
         return cnt;
     }
     
     private void init(String[] grid) {
-        
-        for (int i = 0 ; i < r; i++) {
+        for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                if (grid[i].charAt(j) == 'S')
-                    value[i][j] = 0;
-                else if (grid[i].charAt(j) == 'L')
-                    value[i][j] = -1;
-                else
-                    value[i][j] = 1;
+                if (grid[i].charAt(j) == 'S') {
+                    dir[i][j] = 0;
+                }
+                else if (grid[i].charAt(j) == 'L') {
+                    dir[i][j] = -1;
+                }
+                else {
+                    dir[i][j] = 1;
+                }
             }
         }
     }
